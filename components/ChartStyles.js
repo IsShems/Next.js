b38ed30e9404d5c07c38fc0260eaef2a666e3097
Shems <96719853+IsShems@users.noreply.github.com>
2024-05-06 02:34:30 +0400

    Delete components directory

--- components/ChartStyles.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const ChartContainer = styled.div`
-  max-width: 400px; /* Примерный максимальный размер графика */
-  margin: 0 auto; /* Центрирование графика по горизонтали */
-`;
